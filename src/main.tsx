6b051c59f029a66670048accfce374768c3a6209
Igor Gonçalves <igordossantos38@gmail.com>
2023-04-22 02:10:46 -0300

    feat: import global reset css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './global.scss';
+import './global.reset.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
