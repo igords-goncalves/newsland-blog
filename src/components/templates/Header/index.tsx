b65d78609966d19233074f3a1e51a38051b7836f
Igor Gonçalves <igordossantos38@gmail.com>
2023-04-22 18:45:22 -0300

    refact: change blogs name

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -5,7 +5,7 @@ export const Header = (): JSX.Element => {
         <header className="c-header">
             <div className="u-container u-header__container">
                 <div className="c-header__text u-header__flex">
-                    <h1 className="c-header__logo">Condelândia</h1>
+                    <h1 className="c-header__logo">Newsland</h1>
                     <h2 className="c-header__title">blog</h2>
                 </div>
                 <input
