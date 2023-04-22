975bf022aed1f1e611d27f7c5f5c49d67ab23ea3
Igor Gonçalves <79734585+igords-goncalves@users.noreply.github.com>
2023-04-22 19:23:57 -0300

    Aplicaçao de estilo utilizando sass e criação de component card (#2)
    * style: add more global settings
    
    * style: fix form to bulid a gradient with sass
    
    * refact: use namespace and BEM css to classNames
    
    * feat: build a component header
    
    * style: apply style on header component
    
    * fix: clean style teste from Home component
    
    * style: insert icon on component and apply style
    
    * chore: import icon to project
    
    * style: add reponsivity
    
    * refact: change blogs name
    
    * style: turn utilitary class a global style
    
    * style: turn style from component global
    
    * style: create new variables color
    
    * style: apply style in card component
    
    * style: apply style in main component
    
    * feat: add content children card
    
    * feat: build card component

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,7 +1,20 @@
-export const Header = () => {
+import './style.scss';
+
+export const Header = (): JSX.Element => {
     return (
-        <header>
-            <h1>header</h1>
+        <header className="c-header">
+            <div className="u-container u-header__container">
+                <div className="c-header__text u-header__flex">
+                    <h1 className="c-header__logo">Newsland</h1>
+                    <h2 className="c-header__title">blog</h2>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Pesquise no blog"
+                    id="icon"
+                    className="c-header__input"
+                />
+            </div>
         </header>
     );
 };
