ca218b728025d0e76e1be6a89dfd781c141570e3
Igor Gonçalves <igordossantos38@gmail.com>
2023-04-22 16:05:05 -0300

    feat: build a component header

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,7 +1,19 @@
-export const Header = () => {
+import './style.scss';
+
+export const Header = (): JSX.Element => {
     return (
-        <header>
-            <h1>header</h1>
+        <header className="c-header">
+            <div className="u-container u-header__container">
+                <div className="c-header__text u-header__flex">
+                    <h1 className="c-header__logo">Condelândia</h1>
+                    <h2 className="c-header__title">blog</h2>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Pesquise no blog"
+                    className="c-header__input"
+                />
+            </div>
         </header>
     );
 };
