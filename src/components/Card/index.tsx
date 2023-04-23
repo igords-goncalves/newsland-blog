dd47324983a8f67fc73d9ac13918f17f2d6727b1
Igor Gonçalves <79734585+igords-goncalves@users.noreply.github.com>
2023-04-23 13:51:53 -0300

    Feature add a favorite icon in cards (#4)
    * feat: add favorited icon
    
    * feat: add feature that handle icon to favorite it
    
    * style: add cursos type pointer to icon favorite

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
+
 import './style.scss';
 import favorite from '../../assets/img/favorite-heart.svg';
+import favoriteToggled from '../../assets/img/favorite-toggled.svg';
 
 export const Card = (): JSX.Element => {
+    const [isFavorite, setIsFavorite] = useState(favorite);
+
+    const handleFavoriteIcon = (): string => {
+        setIsFavorite(isFavorite === favorite ? favoriteToggled : favorite);
+        return isFavorite;
+    };
+
+    // TODO: Provisório
+    const date = new Date();
+    const currDate = date.getDate();
+    const currMonth = date.toLocaleString('pt-BR', { month: 'long' });
+    const currYear = date.getFullYear();
+    // ============
+
     return (
         <div className="c-card">
             <header className="c-card__header u-header__flex">
-                <p className="c-card__date">{Date.now()}</p>
-                <img src={favorite} alt="" className="c-card__favorite" />
-                {/* TODO: Adiconar onClick a imagem vpara mudar a cor do icione */}
+                <p className="c-card__date">{`${currDate} de ${currMonth}, ${currYear}`}</p>
+                <img
+                    onClick={handleFavoriteIcon}
+                    src={isFavorite}
+                    alt="Coração"
+                    className="c-card__favorite"
+                />
             </header>
             <h2 className="c-card__title">Lorem, ipsum dolor sit</h2>
             {/* TODO: Acionar o title como link p/ abrir o modal da noticia */}
