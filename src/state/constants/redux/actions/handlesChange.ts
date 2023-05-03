7471913e4390865162c6b2c9244acc221f6ffa4f
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-03 15:51:25 -0300

    feat: add right type to reducer actions

--- src/state/constants/redux/actions/handlesChange.ts
+++ src/state/constants/redux/actions/handlesChange.ts
@@ -1,3 +1,6 @@
-export const handleChange = (e: any, dispatch: any) => {
+export const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    dispatch: React.Dispatch<any>,
+) => {
     dispatch({ type: 'change', payload: e.target.value });
 };
