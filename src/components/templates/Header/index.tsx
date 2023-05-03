86c05890eaef401c2f737b46bac24c95be262910
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-03 14:23:03 -0300

    feat: import reducer modules to component

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,7 +1,18 @@
-import './style.scss';
+import { useReducer } from 'react';
+import { initialState } from '../../../state/constants/redux/initialState';
+import { reducer } from '../../../state/constants/redux/reducer';
 import logo from '../../../assets/img/logo.svg';
+import './style.scss';
+import { handleChange } from '../../../state/constants/redux/actions/handlesChange';
 
 export const Header = (): JSX.Element => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // TODO: O resultado da busca deve ir para o contexto
+    // TODO: Exportar a ation também
+
+    console.log(state);
+
     return (
         <header className="c-header">
             <div className="u-container u-header__container">
@@ -13,6 +24,7 @@ export const Header = (): JSX.Element => {
                     type="text"
                     placeholder="Pesquise no blog"
                     id="icon"
+                    onChange={e => handleChange(e, dispatch)}
                     className="c-header__input"
                 />
             </div>
