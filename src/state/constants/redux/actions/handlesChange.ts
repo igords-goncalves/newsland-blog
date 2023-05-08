a8717e91b4ad82fa90e4df9a8b290fbc28f6332e
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-08 18:06:10 -0300

    chore: deleted files using usereducer

--- src/state/constants/redux/actions/handlesChange.ts
+++
@@ -1,6 +0,0 @@
-export const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    dispatch: React.Dispatch<any>,
-) => {
-    dispatch({ type: 'change', payload: e.target.value });
-};
--- src/state/constants/redux/reducer/index.ts
+++
@@ -1,8 +0,0 @@
-export const reducer = (state: any, action: any) => {
-    switch (action.type) {
-        case 'change':
-            return { ...state, inputValue: action.payload };
-        default:
-            throw new Error();
-    }
-};
