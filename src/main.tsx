acd263878e1fbd50555b3d4a31d94cb15f0956c4
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-08 18:08:03 -0300

    feat: import component Provider from redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './global.scss';
 import './global.reset.scss';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/store/configureStore.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <Provider store={configureStore()}>
+            <App />
+        </Provider>
     </React.StrictMode>,
 );
