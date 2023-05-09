a65a7dd22e5f5426159e61d52f7af4fd828e7a69
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-08 21:34:08 -0300

    feat: change param name to inputValue

--- src/state/hooks/useFuse/useFuse.ts
+++ src/state/hooks/useFuse/useFuse.ts
@@ -1,11 +1,14 @@
 import Fuse from 'fuse.js';
 
 interface FuseTypes {
-    state: string | any;
+    inputValue: string | any;
     options: object;
 }
 
-export const useFuse = (data: object | any, { state, options }: FuseTypes) => {
-    const fuse = new Fuse(data, options).search(state.inputValue);
+export const useFuse = (
+    data: object | any,
+    { inputValue, options }: FuseTypes,
+) => {
+    const fuse = new Fuse(data, options).search(inputValue);
     return fuse;
 };
