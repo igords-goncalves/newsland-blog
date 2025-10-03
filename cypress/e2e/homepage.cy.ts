b825058de0f2d59062e3dbf26a72c983d66b7921
Igor Gonçalves <igordossantos38@gmail.com>
2025-10-02 23:05:08 -0300

    feat: add Cypress configuration and initial test cases for homepage functionality

---
+++ cypress/e2e/homepage.cy.ts
@@ -0,0 +1,55 @@
+import { sampleResults, stubLatestNews } from '../mock/stubLastestNews';
+describe('Visiting page', () => {
+    it('Should visit the page', () => {
+        cy.visit('/');
+    });
+
+    it('Should render the news cards on page', () => {
+        cy.visit('/');
+        cy.get('.c-card').should('exist');
+        cy.get('.c-card').should('be.visible');
+        cy.get('.c-card').should('have.length.greaterThan', 0);
+    });
+
+    it('Should render the news with date, title and description in a card', () => {
+        cy.visit('/');
+        cy.get('[ data-testid="news-date"]').should('exist');
+        cy.get('[ data-testid="news-title"]').should('exist');
+        cy.get('[ data-testid="news-description"]').should('exist');
+    });
+});
+
+describe('Homepage', () => {
+    it('Should reder the news with date, title and description coming from request', () => {
+        stubLatestNews(sampleResults);
+        cy.visit('/');
+        cy.wait('@getLatestNews');
+        // cy.get('[data-testid="news-date"]').should(
+        //     'contain.text',
+        //     parseDate('2025-01-01T00:00:00.000Z'),
+        // );
+        cy.get('[data-testid="news-title"]').should(
+            'contain.text',
+            'Primeira notícia',
+        );
+        cy.get('[data-testid="news-description"]').should(
+            'contain.text',
+            'Descrição da primeira notícia',
+        );
+    });
+
+    it('Should render a fallback message if there are no news', () => {
+        stubLatestNews([]);
+        cy.visit('/');
+        cy.wait('@getLatestNews');
+        cy.get('[data-testid="no-news-message"]')
+            .should('exist')
+            .and('contain.text', 'Nenhuma notícia disponível');
+        cy.get('.c-card').should('not.exist');
+    });
+});
+
+describe('Click on news', () => {
+    // it.todo('Should open a modal');
+    // it.todo('Should render the news content with date, title and content');
+});
